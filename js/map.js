453d4dd609e4ddc702c73b5f76ce646837383fe6
saugusth <saugusth@hawk.iit.edu>
2019-11-12 17:19:46 -0600

    changed code

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
-function initMap(){}
-initMap = function()
+
+function initMap()
 {
 	var options = {
 		zoom = 8,
 		center:{lat:41.8780, lng: -93.0977}
 	}
-	var map = new google.maps.Map(document.getElementById('map'));
+	var map = new google.maps.Map(document.getElementById('map'),options);
 }
