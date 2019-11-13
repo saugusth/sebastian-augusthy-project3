b2c41d1fab720e015acf4f92b223fd121bb13bea
saugusth <saugusth@hawk.iit.edu>
2019-11-12 23:58:30 -0600

    added marker

--- js/map.js
+++ js/map.js
@@ -4,9 +4,10 @@
           center:{lat:47.5515, lng: -101.0020},
           zoom: 5
         });
-      }
 
-      var	marker =  new google.maps.Marker( {
-      	position:{lat:49.2827, lng:-123.1207}
-      	map:map
-      });
+        var	marker =  new google.maps.Marker( {
+      		position:{lat:49.2827, lng: -123.1207},
+      		map: map,
+      		title: 'The Flash'
+      	});
+      }
