36222f54def7016f541743867e26b4a8d2be5737
saugusth <saugusth@hawk.iit.edu>
2019-11-13 13:40:52 -0600

    adedd new code

--- js/map.js
+++ js/map.js
@@ -2,10 +2,10 @@
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center:{lat:47.5515, lng: -101.0020},
-          zoom: 5
+          zoom: 4.5
         });
 
-        var	marker =  new google.maps.Marker( {
+        /*var	marker =  new google.maps.Marker( {
       		position:{lat:49.2827, lng: -123.1207},
       		map: map,
       		title: 'The Flash'
@@ -17,5 +17,16 @@
       	marker.addListener('click',function()
       	{
       		infoWindow.open(map,marker);
-      	});
+      	});*/
+      	//add marker function
+      	addMarker({coords:{lat:49.2827, lng: -123.1207}},
+      	);
+      	addMarker({coords:{lat:41.8781, lng: -87.6298}});
+      	addMarker({coords:{lat:33.7490, lng: -84.3880}});
+      	function addMarker(props){
+					var	marker =  new google.maps.Marker( {
+      			position:props.coords,
+      			map: map
+      		});
+      	}
       }
